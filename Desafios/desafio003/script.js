b59566699bad9f6c1ba1f80cce6061aa6362480b
Guilherme Possebon <gpossebon67@gmail.com>
2022-11-28 20:52:00 -0300

    Arquivos

---
+++ Desafios/desafio003/script.js
@@ -0,0 +1,15 @@
+function hardware() {
+    window.document.location.href = "videohardware.html"
+}
+function html() {
+    window.document.location.href = "html.html"
+}
+function js() {
+    window.document.location.href = "javascript.html"
+}
+function python() {
+    window.document.location.href = "python.html"
+}
+function voltar() {
+    window.document.location.href = "index.html"
+}
